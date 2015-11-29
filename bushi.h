b83052730c6ac3d422cd16e2c52bdca8157e3c4c
Hamilcare <valentin@deuxfleurs.fr>
2015-11-29 12:17:01 +0100

    bushi

--- bushi.h
+++ bushi.h
@@ -1,15 +1,18 @@
+#define         WALLAH(type, cellule, joueur)   ((Bushi){type, cellule, joueur})               
+#define         SINGE(cellule, joueur)          WALLAH(1, *cellule, joueur)
+#define         LION(cellule, joueur)           WALLAH(2, *cellule, joueur)
+#define         DRAGON(cellule, joueur)         WALLAH(3, *cellule, joueur)
+ 
+
 struct Bushi {
 	int type; // 1=Singe, 2=Lion, 3= Dragon	
-	Cellule c; // Cellule du Bushi	
+	*Cellule c; // Cellule du Bushi	
 	int joueur; // Joueur à qui appartient le bushi 	
 	int alive; // 1 = En vie, 0= Hors-jeu
-}
+};
 
-void set_position(Bushi* b, Cellule* c); //Assigne au Bushi b, la cellule c
 
-void set_alive(Bushi* c, int alive); // Renvoie si le Bushi est jouable ou hors-jeu
 
-void set_type(Bushi* c, int type); // Joueur à qui appartient le bushi
 
 void creer_Singe(Bushi *b, Cellule *c, int joueur)
 
